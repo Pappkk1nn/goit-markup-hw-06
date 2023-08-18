660955c1c52de27b76f0f390582685ae08cc0930
Pappkk1nn <139339973+Pappkk1nn@users.noreply.github.com>
2023-08-18 12:56:49 +0300

    1
    1

--- js/modal.js
+++ js/modal.js
@@ -1,28 +1,14 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-    }
-    function showModal(element) {
-      // Get the modal element
-      var modal = document.getElementById(element.dataset.modal);
-    
-      // Show the modal
-      modal.style.display = "block";
-    }
-    $('[data-modal-close]').click(function() {
-      // Get the modal element
-      var modal = $(this).closest('.modal');
-    
-      // Hide the modal
-      modal.hide();
-    });
-  })();
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();

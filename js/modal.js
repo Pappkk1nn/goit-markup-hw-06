2f4072f92b1a030e697c6c6b9d01846d92e32cff
Pappkk1nn <139339973+Pappkk1nn@users.noreply.github.com>
2023-08-18 12:50:15 +0300

    1
    1

--- js/modal.js
+++ js/modal.js
@@ -11,4 +11,18 @@
     function toggleModal() {
       refs.modal.classList.toggle("is-hidden");
     }
+    function showModal(element) {
+      // Get the modal element
+      var modal = document.getElementById(element.dataset.modal);
+    
+      // Show the modal
+      modal.style.display = "block";
+    }
+    $('[data-modal-close]').click(function() {
+      // Get the modal element
+      var modal = $(this).closest('.modal');
+    
+      // Hide the modal
+      modal.hide();
+    });
   })();
